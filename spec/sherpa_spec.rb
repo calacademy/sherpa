ab6a41a7d1888a8a59f1c4e572e28c40ac6e0c6c
Mark Wilden <mark@mwilden.com>
2012-02-20 06:51:10 -0800

    Silent running, progress messages, series_volume_issue

--- spec/sherpa_spec.rb
+++ spec/sherpa_spec.rb
@@ -5,36 +5,28 @@ require 'citrus'
 describe Sherpa do
   before() {@parser = Sherpa}
 
-  def run_spec citation, title, year, pages
+  def run_spec citation, title, series_volume_issue, year, pages
     @parser.parse(citation).should == {
-      title: title, year: year, pages: pages, text: citation
+      title: title, year: year, series_volume_issue: series_volume_issue, pages: pages, text: citation
     }
   end
 
   describe "Parsing different kinds of title" do
     it "should handle a title ending with an abbreviated word" do
-      run_spec 'Suppl. Ent. Syst. 1798, 435', 'Suppl. Ent. Syst.',  '1798', '435',
+      run_spec 'Suppl. Ent. Syst. 1798, 435', 'Suppl. Ent. Syst.', nil, '1798', '435',
     end
     it "should handle a title ending with an unabbreviated word" do
-      run_spec 'Gen. Sp. Ins. Geer, 1783, 32.', 'Gen. Sp. Ins. Geer', '1783', '32'
+      run_spec 'Gen. Sp. Ins. Geer, 1783, 32.', 'Gen. Sp. Ins. Geer', nil, '1783', '32'
     end
   end
 
-  #it "should handle a journal with a series/volume/issue" do
-    #Sherborn.parse('Uitl. Kapellen, I (8) 1776, 146').value.should == {
-      #title: 'Uitl. Kapellen',
-      #series_volume_issue: 'I (8)',
-      #year: '1776',
-      #pages: '146',
-    #}
-  #end
-
-  #it "requires an abbreviated word to begin with a capital letter" do
-    #-> {Sherborn.parse('rt.', root: :abbreviated_word)}.should raise_error Citrus::ParseError
-  #end
-
-  #it "requires an abbreviated word to end with a period" do
-    #-> {Sherborn.parse('Rt', root: :abbreviated_word)}.should raise_error Citrus::ParseError
-  #end
+  describe "Series, volume, issue" do
+    it "should handle a journal with a volume" do
+      run_spec 'Danske Atlas, I 1763, 621', 'Danske Atlas', 'I', '1763', '621'
+    end
+    it "should handle a journal with a volume" do
+      run_spec 'Mant. I. 1787, 164', 'Mant.', 'I', '1787', '164'
+    end
+  end
 
 end
