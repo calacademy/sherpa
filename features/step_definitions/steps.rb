535d56de54282fbb811d8aa33c76fde77b917884
Mark Wilden <mark@mwilden.com>
2012-02-20 03:48:46 -0800

    Rename steps.rb

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,5 @@
 Then "it should emit the JSON:" do |json|
-  match = all_output.match('\[^\]]+')
+  match = all_output.match /\[.*\]+/
   match.should_not be_nil
   JSON.parse(match[0]).should == JSON.parse(json)
 end
