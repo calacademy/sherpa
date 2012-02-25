4c09a45a5bf795b636c1711c820756705bf48f5f
Mark Wilden <mark@mwilden.com>
2012-02-25 15:51:15 -0800

    Sort the comparison list

--- lib/sherpa.rb
+++ lib/sherpa.rb
@@ -64,7 +64,7 @@ module Sherpa
         </head>
         <body>
       EOS
-      for citation in citations
+      for citation in citations.sort_by{|a| a[:citation]}
         our_cells = []
         their_cells = []
         any_different = false
