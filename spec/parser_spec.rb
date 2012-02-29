006de4e9059fb1eadf07d75d6e5c56aa7351dbaa
Mark Wilden <mark@mwilden.com>
2012-02-28 16:11:06 -0800

    Linnaeus

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -17,7 +17,10 @@ describe Sherpa::Parser do
     end
 
     describe "Exemplars" do
-      it "should handle the canonical case of TITLE VOLUME DATE, PAGE" do
+      it "should handle Linnaeus" do
+        parse_and_check 'Linn. Syst. Nat., ed. 13, I. 1789, 1849.', 'Linn. Syst. Nat., ed. 13', 'I.', '1789', '1849'
+      end
+      it "should handle TITLE VOLUME DATE, PAGE" do
         parse_and_check 'Ent. Syst. IV. 1794, 262', 'Ent. Syst.', 'IV.', '1794', '262'
       end
       it "should handle TITLE, DATE, PAGE (no volume; title does not end in a period)" do
@@ -26,13 +29,16 @@ describe Sherpa::Parser do
       it "should handle TITLE VOLUME (ISSUE) DATE, PAGE" do
         parse_and_check 'Uitl. Kapellen, I. (8) 1776, 146', 'Uitl. Kapellen', 'I. (8)', '1776', '146'
       end
+      it "should handle multipart citations where both parts are complete" do
+        parser.parse('Danske Atlas, I. 1763, 621 ; & Danischer Atlas, I. 1765, 401.').should == {
+          citations: [
+            {title: 'Danske Atlas', volume: 'I.', date: '1763', pages: '621'},
+            {title: 'Danischer Atlas', volume: 'I.', date: '1765', pages: '401'},
+          ]
+        }
+      end
     end
   end
-
-      #it "should parse a title (ending in unabbrev. word) + year + page" do
-        #parse_and_check 'Gen. Sp. Ins. Geer, 1783, 32.', 'Gen. Sp. Ins. Geer', nil, '1783', '32.'
-      #end
-    #end
 
     #describe "Specific combinations or patterns of data" do
       #it "should handle a bracketed year range without a trailing comma followed by a plate section in brackets" do
@@ -143,112 +149,112 @@ describe Sherpa::Parser do
 
   describe "Component rules" do
 
-    describe "New stuff (remove this describe)" do
-      describe "Title" do
-        it "should handle a title ending with an unabbreviated word" do
-          grammar.parse 'Gen. Sp. Ins. Geer', root: :title
-        end
-        it "should handle a title ending with an abbreviated word" do
-          grammar.parse 'Suppl. Ent. Syst.', root: :title
-        end
-        it "should handle a title with an accented character" do
-          grammar.parse 'Ency. Méth.', root: :title
-        end
-        it "should not include the volume in the title" do
-          -> {grammar.parse 'Ent. Syst. IV.', root: :title}.should raise_error Citrus::ParseError
-        end
-        it "should not include the volume in the title" do
-          -> {grammar.parse 'Ent. Syst. IV', root: :title}.should raise_error Citrus::ParseError
-        end
+    describe "Title" do
+      it "should handle a title ending with an unabbreviated word" do
+        grammar.parse 'Gen. Sp. Ins. Geer', root: :title
       end
-
-      describe "Volume" do
-        it "should handle a roman number + period" do
-          grammar.parse 'I.', root: :volume_without_issue
-          grammar.parse 'I.', root: :volume
-        end
-        it "should handle a roman number + (issue)" do
-          grammar.parse 'I. (1)', root: :volume_with_issue
-          grammar.parse 'I. (1)', root: :volume
-        end
-        it "should handle a roman number without a period" do
-          grammar.parse 'I', root: :volume_without_issue
-          grammar.parse 'I', root: :volume
-        end
-        it "should handle a roman number without a period followed by the issue" do
-          grammar.parse 'I (—)', root: :volume_without_period_with_issue
-          grammar.parse 'I (—)', root: :volume
-        end
-        it "should handle any number in the issue" do
-          grammar.parse '(43)', root: :issue
-        end
-        it "should an issue without a volume" do
-          grammar.parse '(43)', root: :volume
-        end
-        it "should handle a placeholder issue" do
-          grammar.parse '(-)', root: :issue
-        end
-        it "should handle a placeholder issue" do
-          grammar.parse '(—)', root: :issue
-        end
+      it "should handle a title ending with an abbreviated word" do
+        grammar.parse 'Suppl. Ent. Syst.', root: :title
       end
-
-      describe "Date" do
-        it "should handle a year" do
-          parser.parse "1840", root: :year
-          parser.parse "1840", root: :date
-        end
-        it "should handle an abbreviated month + year" do
-          parser.parse "Oct. 1840", root: :date
-        end
-        #it "should include a bracketed phrase following a date in the date" do
-          #parser.parse "1840-46 [<em>vero propius</em> 1847]", root: :date
-        #end
-        #it "should handle a one-digit end year in a range" do
-          #parser.parse "1830-2", root: :date
-        #end
-        #it "should handle a bracketed date" do
-          #parser.parse "[1830-2]", root: :date
-        #end
-        #it "should handle x or y" do
-          #parser.parse '1830 <em>vel</em> 1831', root: :date
-        #end
-        #it "should handle a year with open last digit" do
-          #parser.parse '184-', root: :date
-        #end
+      it "should handle a title with an accented character" do
+        grammar.parse 'Ency. Méth.', root: :title
       end
-
-    end
-
-    describe "Pages" do
-      it "should handle a reference to a section of a plate" do
-        parser.parse "Volutes, pl. ii", root: :pages
+      it "should not include the volume in the title" do
+        -> {grammar.parse 'Ent. Syst. IV.', root: :title}.should raise_error Citrus::ParseError
       end
-      it "should handle a reference to another section of a plate" do
-        parser.parse "Turbinellus, pl. iii", root: :pages
+      it "should not include the volume in the title" do
+        -> {grammar.parse 'Ent. Syst. IV', root: :title}.should raise_error Citrus::ParseError
       end
-      it "should handle an arabic plate number" do
-        parser.parse "Buccins, pl. 1", root: :pages
-      end
-      it "should handle a plate in parentheses" do
-        parser.parse "(pl. 17)", root: :pages
+      it "should handle Linnaeus" do
+        grammar.parse 'Linn. Syst. Nat., ed. 13', root: :title
       end
     end
 
-    describe "Roman numerals" do
-      it "should understand these" do
-        for number in ['III', 'I', 'II', 'IV', 'V']
-          parser.parse number, root: :roman_number
-        end
-        for number in ['iii', 'i', 'ii', 'iv', 'v']
-          parser.parse number, root: :roman_number
-        end
+    describe "Volume" do
+      it "should handle a roman number + period" do
+        grammar.parse 'I.', root: :volume_without_issue
+        grammar.parse 'I.', root: :volume
       end
-      it "should not use mixed case" do
-        -> {parser.parse "Ic", root: :roman_number}.should raise_error Citrus::ParseError
+      it "should handle a roman number + (issue)" do
+        grammar.parse 'I. (1)', root: :volume_with_issue
+        grammar.parse 'I. (1)', root: :volume
+      end
+      it "should handle a roman number without a period" do
+        grammar.parse 'I', root: :volume_without_issue
+        grammar.parse 'I', root: :volume
+      end
+      it "should handle a roman number without a period followed by the issue" do
+        grammar.parse 'I (—)', root: :volume_without_period_with_issue
+        grammar.parse 'I (—)', root: :volume
+      end
+      it "should handle any number in the issue" do
+        grammar.parse '(43)', root: :issue
+      end
+      it "should an issue without a volume" do
+        grammar.parse '(43)', root: :volume
+      end
+      it "should handle a placeholder issue" do
+        grammar.parse '(-)', root: :issue
+      end
+      it "should handle a placeholder issue" do
+        grammar.parse '(—)', root: :issue
       end
     end
 
+    describe "Date" do
+      it "should handle a year" do
+        parser.parse "1840", root: :year
+        parser.parse "1840", root: :date
+      end
+      it "should handle an abbreviated month + year" do
+        parser.parse "Oct. 1840", root: :date
+      end
+      #it "should include a bracketed phrase following a date in the date" do
+        #parser.parse "1840-46 [<em>vero propius</em> 1847]", root: :date
+      #end
+      #it "should handle a one-digit end year in a range" do
+        #parser.parse "1830-2", root: :date
+      #end
+      #it "should handle a bracketed date" do
+        #parser.parse "[1830-2]", root: :date
+      #end
+      #it "should handle x or y" do
+        #parser.parse '1830 <em>vel</em> 1831', root: :date
+      #end
+      #it "should handle a year with open last digit" do
+        #parser.parse '184-', root: :date
+      #end
+    end
+
+  end
+
+  describe "Pages" do
+    it "should handle a reference to a section of a plate" do
+      parser.parse "Volutes, pl. ii", root: :pages
+    end
+    it "should handle a reference to another section of a plate" do
+      parser.parse "Turbinellus, pl. iii", root: :pages
+    end
+    it "should handle an arabic plate number" do
+      parser.parse "Buccins, pl. 1", root: :pages
+    end
+    it "should handle a plate in parentheses" do
+      parser.parse "(pl. 17)", root: :pages
+    end
+  end
+
+  describe "Roman numerals" do
+    it "should understand these" do
+      for number in ['III', 'I', 'II', 'IV', 'V']
+        parser.parse number, root: :roman_number
+      end
+      for number in ['iii', 'i', 'ii', 'iv', 'v']
+        parser.parse number, root: :roman_number
+      end
+    end
+    it "should not use mixed case" do
+      -> {parser.parse "Ic", root: :roman_number}.should raise_error Citrus::ParseError
+    end
   end
 
 end
