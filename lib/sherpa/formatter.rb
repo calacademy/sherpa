23d6ebef7700f62fb3e1715f914f45886c735b96
Mark Wilden <mark@mwilden.com>
2012-03-04 08:06:50 -0800

    Copy stylesheet into HTML. Header, explanation.

--- lib/sherpa/formatter.rb
+++ lib/sherpa/formatter.rb
@@ -4,9 +4,21 @@ module Sherpa::Formatter
     <html>
       <head>
         <meta http-equiv='Content-Type' content='text/html; charset=utf-8'></head>
-        <link href="sherpa.css" media="screen" rel="stylesheet" type="text/css" />
+    EOS
+    string << '<style>' << File.read(File.dirname(__FILE__) + '/../../data/sherpa.css') << '</style>'
+    string << <<-EOS
       </head>
       <body>
+      <h1>Some results of parsing micro-citations in Sherborn's <i>Index Animalium</i> using the Sherpa parser</hi>
+      <h2>Mark Wilden<h2>
+      <h3>California Academy of Sciences</h3>
+      <h4>March 4, 2012</h4>
+      <p>
+        This compares Sherpa's results with Rich Pyle's manual parse of ~1400 records.
+        Sherborn is in grey, then comes Sherpa's parse, then Rich's.
+        The columns are: title, series/volume/issue, date, and pages.
+      </p>
+      <p>Currently, Sherpa parses 82% of Rich's data set and 75% of <i>Index Animalium</i> as a whole.
     EOS
     for citation in citations.sort_by{|a| a[:citation]}.uniq
       our_cells = []
